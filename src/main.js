5f81d0ae67fc6311806a3e1c483a6024054d6a80
Karl Ranna <karl@karlranna.com>
2016-04-10 22:01:17 +0300

    Remove commented out code

--- src/main.js
+++ src/main.js
@@ -1,29 +1,3 @@
-/*
-import Rx from 'rx';
-import Cycle from '@cycle/core';
-import {makeDOMDriver, div, input, p} from '@cycle/DOM';
-
-function main(drivers) {
-  return {
-    DOM: drivers.DOM.select('input').events('click')
-      .map(ev => ev.target.checked)
-      .startWith(false)
-      .map(toggled =>
-        div([
-          input({type: 'checkbox'}), 'Toggle me',
-          p(toggled ? 'TURNED ON' : 'TURNED OFF')
-        ])
-      )
-  };
-}
-
-const drivers = {
-  DOM: makeDOMDriver('#app')
-};
-
-Cycle.run(main, drivers);
-*/
-
 import Cycle from '@cycle/core'
 import {Observable} from 'rx'
 import {div, ul, li, makeDOMDriver} from '@cycle/DOM'
